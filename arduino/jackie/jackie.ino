b987402ac6672383442cdd568fcf1ea2c022edb2
padr31 <pali31pd@centrum.sk>
2016-03-26 17:51:27 +0100

    arduino: added servo controlling

--- arduino/jackie/jackie.ino
+++ arduino/jackie/jackie.ino
@@ -1,11 +1,61 @@
+#include <Servo.h>
+
+#define pin_l 10
+#define pin_r 11
+
+class ServoController;
+
+class ServoController{
+public:
+  Servo l;
+  Servo r;
+  ServoController() {
+    l.attach(pin_l);
+    r.attach(pin_r);
+  }
+  
+  void movel(int angle){
+    l.write(angle);
+    Serial.print("Moving l with angle ");
+    Serial.print(angle);
+    Serial.println();
+  }
+  
+  void mover(int angle){
+    r.write(angle);
+    Serial.print("Moving r with angle ");
+    Serial.print(angle);
+    Serial.println();
+  }
+};
+
+ServoController servoController;
 
 void execute(String cmd){
-  Serial.println(cmd);
+  int space = cmd.indexOf(" ");
+  String first = cmd.substring(0, space);
+
+  cmd = cmd.substring(space+1);
+  
+  switch(first.toInt()){
+  case 1:
+    space = cmd.indexOf(" ");
+    int angle = cmd.substring(0, space).toInt();
+    cmd = cmd.substring(space+1);
+    String side = cmd.substring(0, 1);
+    if(side == "l"){
+      servoController.movel(angle);
+    } else if(side == "r"){
+      servoController.mover(angle);
+    }
+    break;
+  }
 }
 
 void setup() {
   Serial.begin(115200);
 }
+
 
 
 void loop() {
