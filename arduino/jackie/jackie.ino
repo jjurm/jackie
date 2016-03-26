ec2b44633586bff8c58082768b9fca278831f5b0
padr31 <pali31pd@centrum.sk>
2016-03-26 16:20:19 +0100

    arduino: serial read is working(by pavoldr31 & miso)

--- arduino/jackie/jackie.ino
+++ arduino/jackie/jackie.ino
@@ -1,79 +1,28 @@
-using namespace std;
 
-class SerialCommander;
-class Command;
-
-/*
- Comamnd utility class
- */
-class Command {
-public:
-  char data[10];
-  int length = 0;
-  Command(){
-    clean();
-  }
-
-  void clean() {
-    for(int i = 0; i < 10; i++) {
-      data[i] = 0;
-    }
-    length = 0;
-  }
-  
-  bool append(char c) {
-    data[length] = c;
-    length++;
-  }
-  
-  void send() {
-    for (int i = 0; i < length; i++) {
-      Serial.print(data[i]);
-      if(i < length - 1){
-        Serial.print(" ");
-      } else {
-        Serial.println(";");   
-      }      
-    }
-  }
-};
-
-class SerialCommander {
-public:
-  
-  void send(Command command) {
-    command.send();
-  }
-  
-  Command receive() {
-    int cSize = 10;
-    char c[cSize];
-    Serial.readBytesUntil(';', c, cSize);
-    Command command;
-    for(int i = 0; i < cSize; i++) {
-      command.append(c[i]);
-    }
-    return command;
-  }
-  
-  void execute(Command command) {
-    for(int i = 0; i < command.length;i++){
-      Serial.print(command.data[i]);
-    }
-    Serial.println(";");
-  }
-};
+void execute(String cmd){
+  Serial.println(cmd);
+}
 
 void setup() {
   Serial.begin(115200);
 }
 
-SerialCommander serialCommander;
 
 void loop() {
+  String stack = "";
+  int index = 0;
+  String cmd = "";
   if(Serial.available()){
-  Command command = serialCommander.receive();
-  serialCommander.execute(command);
+    stack.concat(Serial.readString());
+  }
+  while(true){
+    index = stack.indexOf(";")+1;
+    cmd = stack.substring(0, index);
+    stack = stack.substring(index);
+    if(cmd != "") execute(cmd);
+    if(stack == ""){
+      break;
+    }
   }
 }
 
